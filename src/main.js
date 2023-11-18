a2321a2f50ba79c71665aac5829acd38e881806c
唐无忌 <tangwuji20040212@163.com>
2023-11-18 15:34:19 +0800

    引入el-icon组件

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
@@ -10,5 +10,7 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+  }
 app.mount('#app')
