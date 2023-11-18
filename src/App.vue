b23f9e8b7850cb5b3b49daf8b371efd17c539a91
唐无忌 <tangwuji20040212@163.com>
2023-11-18 15:14:42 +0800

    路由框架搭建

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,8 @@
 <script setup>
 import { RouterView } from 'vue-router'
-import { useUserStore } from '@/stores/userStore'
-const userStore = useUserStore() // 创建一个vue3的store实例
 </script>
 
 <template>
-  <div>
-    vue3项目启动！
-   <test-dome></test-dome>
-   <h2>token:{{ userStore.token }}</h2>
-    <el-button type="success" @click="userStore.setToken('123456')">设置token</el-button>
-    <el-button type="warning" @click="userStore.removeToken()">移除token</el-button>
-  </div>
   <RouterView/>
 </template>
 
