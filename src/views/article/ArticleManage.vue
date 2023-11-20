8741a9dc4d44bf603ccc70b11f1fbb5402e44077
唐无忌 <tangwuji20040212@163.com>
2023-11-20 23:30:15 +0800

    添加文章封面时上传校验和本地预览

--- src/views/article/ArticleManage.vue
+++ src/views/article/ArticleManage.vue
@@ -6,6 +6,27 @@ const onReset = () => {
 }
 const showDrawer = ref(false)
 
+// 添加文章封面时上传校验本地预览
+const imageUrl = ref('')
+// 当用户添加封面时
+const onAvatarChange = (uploadFile) => {
+
+    if (beforeUpload(uploadFile)) { // 会根据文件的大小返回true或false
+        imageUrl.value = URL.createObjectURL(uploadFile.raw)
+        // 前端图片上传本地预览惯用语法——URL.createObjectURL(file)
+        return true
+    }
+    // 图片过大
+    return false
+}
+// 选择封面前校验
+const beforeUpload = (rawFile) => {
+    if (rawFile.size / 1024 / 1024 > 2) {
+        ElMessage.error('图片大小不能超过 2MB!');
+        return false;
+    }
+    return true;
+};
 </script>
 <template>
     <page-container title="文章管理">
@@ -46,8 +67,44 @@ const showDrawer = ref(false)
     </page-container>
     <!-- 右侧抽屉 -->
     <el-drawer v-model="showDrawer" title="添加文章" direction="rtl" size="50%">
-        Hi, there!
+        添加封面：
+        <el-upload class="avatar-uploader" :show-file-list="false" :on-change="onAvatarChange" :auto-upload="false">
+            <!--  :auto-upload="false" 将自动上传设置为false -->
+            <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+            <el-icon v-else class="avatar-uploader-icon">
+                <Plus />
+            </el-icon>
+        </el-upload>
     </el-drawer>
 </template>
     
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.avatar-uploader .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+}
+</style>
+<!-- 设置上传封面样式 -->
+<style lang="scss">
+.avatar-uploader .el-upload {
+    border: 1px dashed var(--el-border-color);
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+    transition: var(--el-transition-duration-fast);
+}
+
+.avatar-uploader .el-upload:hover {
+    border-color: var(--el-color-primary);
+}
+
+.el-icon.avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    text-align: center;
+}
+</style>
